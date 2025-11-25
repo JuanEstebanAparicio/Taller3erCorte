9a13cf0abc9b5a7716032b8b1db7ba7912512e5a
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-11-24 19:19:56 -0500

    commit con el proyecto en blanco

---
+++ src/main/java/com/aparicio/calculadora/ci/cd/CalculadoraCiCd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aparicio.calculadora.ci.cd;
+
+/**
+ *
+ * @author Juanest15
+ */
+public class CalculadoraCiCd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
